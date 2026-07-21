d2e6ad0b20f33ab0e3ba1fbe8624f92d87d00f78
chu23465 <130033130+chu23465@users.noreply.github.com>
2026-07-21 22:30:28 +0530

    Force again

--- script4.js
+++ script4.js
@@ -4,27 +4,9 @@
     const LEVEL = levelMatch ? parseInt(levelMatch[1]) : null;
     const MAX_LEVEL = 34;
     let GITHUB = true;
-    if (GITHUB === true) {document.head.innerHTML.replace(`<link rel='shortcut icon' href='https://overthewire.org/img/favicon.ico' />
-  <link rel="stylesheet" href="https://overthewire.org/css/style.css" type="text/css" media="screen" />
-  <link href="https://fonts.googleapis.com/css?family=Inconsolata|Pontano+Sans|Maven+Pro&subset=latin,latin-ext" rel='stylesheet' type='text/css' />
-  <link rel="stylesheet" href="https://overthewire.org/css/highlightcode.css" />
-  <link rel="stylesheet" href="https://overthewire.org/css/patreon.css" />
-  <script src="https://overthewire.org/js/jquery-3.7.1.min.js"></script>
-  <script src="https://overthewire.org/js/mustache-4.0.0.min.js"></script>
-  <script src="https://overthewire.org/js/highlight.min.js"></script>
-  <script src="https://overthewire.org/js/otwcrap.js"></script>
-  <script src="https://overthewire.org/js/updatedmarkers.js"></script>`,
-`<link rel='shortcut icon' href='https://cdn.jsdelivr.net/gh/chu23465/overthewire_bandit_UI/img/favicon.ico' />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/chu23465/overthewire_bandit_UI/css/style.css" type="text/css" media="screen" />
-  <link href="https://fonts.googleapis.com/css?family=Inconsolata|Pontano+Sans|Maven+Pro&subset=latin,latin-ext" rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/chu23465/overthewire_bandit_UI/css/highlightcode.css" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/chu23465/overthewire_bandit_UI/css/patreon.css" />
-  <script src="https://cdn.jsdelivr.net/gh/chu23465/overthewire_bandit_UI/js/jquery-3.7.1.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/gh/chu23465/overthewire_bandit_UI/js/mustache-4.0.0.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/gh/chu23465/overthewire_bandit_UI/js/highlight.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/gh/chu23465/overthewire_bandit_UI/js/otwcrap.js"></script>
-  <script src="https://cdn.jsdelivr.net/gh/chu23465/overthewire_bandit_UI/js/updatedmarkers.js"></script>`);
-}
+    if (GITHUB === true) {
+        document.head.innerHTML.replaceAll("https://overthewire.org", "https://cdn.jsdelivr.net/gh/chu23465/overthewire_bandit_UI");
+    }
     /* grab sidemenu level links before wiping the page (present on index + level pages) */
     const sideLinks = [...document.querySelectorAll('a[href*="bandit"]')]
         .filter(a => /bandit\d+\.html$/.test(a.getAttribute('href') || ''))
